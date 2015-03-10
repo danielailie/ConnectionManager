b141bc7f98e6ba83834f30476224010179dd6bfd
danielailie12 <danielailie12@gmail.com>
2015-03-10 16:48:49 +0200

    ConnectionManager added(exercise 6) not finished

---
+++ ConnectionManager/ConnectionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionManager
+{
+    class ConnectionManager
+    {
+        Connection connection=new Connection();
+        static List<Connection> connections= new List<Connection>();
+
+        public static void GetConnections()
+        {
+            foreach (var c in connections)
+            {
+                
+            }
+        }
+    }
+}
